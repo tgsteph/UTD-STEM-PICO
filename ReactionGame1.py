939d5108e66c735c777f72a33fb5b17e75e3771f
Tim Stephens <104217489+tgsteph@users.noreply.github.com>
2022-04-29 00:45:49 -0500

    Create ReactionGame1.py
    (Chapter 4) Reaction Game for One Person

---
+++ ReactionGame1.py
@@ -0,0 +1,21 @@
+import machine
+import utime
+import urandom
+
+pressed = False
+led = machine.Pin(15, machine.Pin.OUT)
+button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+def button_handler(pin):
+  global pressed
+  if not pressed:
+    pressed=True
+    timer_reaction = utime.ticks_diff(utime.ticks_ms(), timer_start)
+    print("Your reaction time was " + str(timer_reaction) + " milliseconds!")
+    
+led.value(1)
+utime.sleep(urandom.uniform(5, 10))
+led.value(0)
+
+timer_start = utime.ticks_ms()
+button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
